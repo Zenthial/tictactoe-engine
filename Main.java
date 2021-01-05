b0cca66c6c24f8265956f11d37123ab528166e62
Zenthial <iamzenthial@gmail.com>
2021-01-05 01:20:24 -0500

    more explanation comments

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
     public static void main(String[] args) {
+        /*
+            The javadoc comments exist so I can track what function returns what in VSCode, since thats where this was written
+            If you're viewing this in Repl.it, it sadly does not support javadoc for some odd reason
+        */
+
         Board gameBoard = new Board();
 
         System.out.println(gameBoard.SetTile("X", 1, 2));
--- Position.java
+++ Position.java
@@ -7,6 +7,7 @@ public class Position {
         this.y = y;
     }
 
+    /**simple get method similar to C# variable access modifiers @return int[x, y] */
     public int[] Get() {
         return new int[] {this.x, this.y};
     }
